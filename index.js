e6f3bd22b40e7e7852678420605b28afd4091245
dlochrie <dlochrie@somethingelse.com>
2013-04-05 23:04:34 -0500

    Fixed issue where pngs and gifs werent being uploaded. Made some minor syntax changes.

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ var fs = require('fs'),
 	path = require('path'),
 	url = require("url"),
 	gd = require('node-gd'), 
-	mime = require('mime'),
-	async = require('async');
+	mime = require('mime');
 
 /**
- * TODO: Verify this is a complete list
- * Should be CONST: var MIMES_ALLOWED
+ * CONST: MIMES_ALLOWED
  */
-var mimes_allowed = [
+var MIMES_ALLOWED = [
 	"image/gif",
 	"image/jpeg",
 	"image/png"
@@ -36,12 +34,10 @@ Picsee.prototype.initialize = function (options) {
   options = options || {};
 	self._stagingDir = options.stagingDir || false;
 	self._processDir = options.processDir || false;
-	self._uploadDir = options.uploadDir || false;
 	self._versions = options.versions || false;
 	self._separator = options.separator || false;
 	self._namingConvention = options.namingConvention || false;
 	self._inputFields = options.inputFields || [];
-	//console.log('conf', self);
 	return self;
 }
 
@@ -62,17 +58,14 @@ Picsee.prototype.initialize = function (options) {
  */ 
 
 Picsee.prototype.upload = function (req, res, cb) {
-	var self = this;
-
-	// Check to see if file is an acceptable image
-	var allowed = self._inputFields; // TODO: Better var name???
-
-	var photos = [];
-	var results = [];
+	var self = this,
+		allowedInputs = self._inputFields, 
+		photos = [],
+		results = [];
 
 	// Add each approved input into the queue for processing 
 	for (var file in req.files) {
-		if (allowed.indexOf(file) !== -1) {
+		if (allowedInputs.indexOf(file) !== -1) {
 			photos.push(req.files[file]);
 		}
 	}
@@ -88,9 +81,7 @@ Picsee.prototype.upload = function (req, res, cb) {
 			return cb(null, results);
 		}
 	}
-
 	preprocess(photos.shift());
-
 }
 
 Picsee.prototype.crop = function (req, res) {
@@ -110,7 +101,6 @@ Picsee.prototype.crop = function (req, res) {
  * (b) send to process each version
  */
 Picsee.prototype.preprocess = function (image, cb) {
-
 	var self = this,
 		oldName = image.name,
 		ext = getFileExt(oldName),
@@ -129,7 +119,7 @@ Picsee.prototype.preprocess = function (image, cb) {
 			if (err) return cb('Cannot save file: ' + stagingPath, null);
 			// TODO: Delete file. 
 			var mime = getMime(stagingPath);
-			if (mimes_allowed.indexOf(mime) !== -1) {
+			if (MIMES_ALLOWED.indexOf(mime) !== -1) {
 				self.process(opts, cb);
 			} else {
 				return cb('This file is NOT an image: ' + oldName, null); 
@@ -163,6 +153,7 @@ Picsee.prototype.process = function (opts, cb) {
 			var params = {
 				stagingPath: opts.stagingPath,
 				processPath: self._processDir + rename,
+				imageName: rename,
 				ext: ext,
 				w: version[versionName].w || 0,
 				h: version[versionName].h || 0,
@@ -292,6 +283,7 @@ function prepareOptions (post) {
 /**
  * This method takes the staging file and creates a resized one
  * from it.
+ * `80` describes the quality we are saving it at. 
  */ 
 function resizeJpeg (opts, cb) {
 	var src = gd.createFromJpeg(opts.stagingPath),
@@ -303,30 +295,50 @@ function resizeJpeg (opts, cb) {
 	src.copyResampled(target, 0, 0, 0, 0, dims.w, dims.h, src.width, src.height);
 	target.saveJpeg(opts.processPath, 80, function (err) { 
 		if (err) return cb('Could not save processed image: ' + opts.processPath, null); 
-		return cb(null, true);
+		return cb(null, { status: 'success', image: opts.imageName, 
+			path: opts.processPath });
 	});
 }
 
-function resizeGif (img, w, h) {
-	w = (w) ? w : false;
-	h = (h) ? h : false;
-	var src = gd.createFromGif(img);
-	if (w) rescaleFromWidth(w, src.width ,src.height);
-	if (h) rescaleFromHeight(h, src.width ,src.height);
-	var target = gd.createTrueColor(w, h);
-	src.copyResampled(target, 0, 0, 0, 0, w, h, src.width,src.height);
-	target.saveGif(img, 80);
+/**
+ * This method takes the staging file and creates a resized one
+ * from it.
+ * `80` describes the quality we are saving it at. 
+ */
+function resizeGif (opts, cb) {
+	var src = gd.createFromGif(opts.stagingPath),
+		w = (opts.w) ? opts.w : false,
+		h = (opts.h) ? opts.h : false,
+		dims = parseDimensions(w, h, src.width, src.height);
+
+	var target = gd.createTrueColor(dims.w, dims.h);
+	src.copyResampled(target, 0, 0, 0, 0, dims.w, dims.h, src.width, src.height);
+	target.saveGif(opts.processPath, 80, function (err) { 
+		if (err) return cb('Could not save processed image: ' + opts.processPath, null); 
+		return cb(null, { status: 'success', image: opts.imageName, 
+			path: opts.processPath });
+	});
 }
 
-function resizePng (img, w, h) {
-	w = (w) ? w : false;
-	h = (h) ? h : false;
-	var src = gd.createFromPng(img);
-	if (w) h = rescaleFromWidth(w, src.width ,src.height);
-	if (h) w = rescaleFromHeight(h, src.width ,src.height);
-	var target = gd.createTrueColor(w, h);
-	src.copyResampled(target, 0, 0, 0, 0, w, h, src.width,src.height);
-	target.savePng(img, 9);
+/**
+ * This method takes the staging file and creates a resized one
+ * from it.
+ * `9` describes the quality we are saving it at - PNGs are different
+ * than Gifs and Jpegs. 
+ */
+function resizePng (opts, cb) {
+	var src = gd.createFromPng(opts.stagingPath),
+		w = (opts.w) ? opts.w : false,
+		h = (opts.h) ? opts.h : false,
+		dims = parseDimensions(w, h, src.width, src.height);
+
+	var target = gd.createTrueColor(dims.w, dims.h);
+	src.copyResampled(target, 0, 0, 0, 0, dims.w, dims.h, src.width, src.height);
+	target.savePng(opts.processPath, 9, function (err) { 
+		if (err) return cb('Could not save processed image: ' + opts.processPath, null); 
+		return cb(null, { status: 'success', image: opts.imageName, 
+			path: opts.processPath });
+	});
 }
 
 function parseDimensions (w, h, sw, sh) {
