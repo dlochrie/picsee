97aae7c8d86f89e12779f6b51760664150fd94a2
dlochrie <dlochrie@somethingelse.com>
2013-04-10 19:51:29 -0500

    Added some testing stuff which one day hopefully I can expand on.

--- test/index.js
+++ test/index.js
@@ -1,7 +1,52 @@
-var should = require('should');
-var picsee = require('../index.js');
-var testImg = 'test/picsee.png';
+var picsee = require('../'),
+	should = require('should');
 
-should.exist(picsee);
-picsee.should.have.property('stat');
-picsee.stat.should.be.an.instanceof(Function);
+// Set up upload paths
+var root = __dirname + '/test/';
+var staging = root + 'images/staging/';
+var processing = root + 'images/processing/';
+var uploaded = root + 'images/uploaded/';
+
+// Setup Options
+var options = {
+	docRoot: root,
+	urlRoot: 'http://some-app.com/',
+	stagingDir: staging,
+	processDir: processing,
+	uploadDir: uploaded,
+	versions: [  
+		{ "thmb": { w: 64, h: 64 } },   
+		{ "profile": { w: 200, h: null } },  
+		{ "full": { w: null, h: null } }  
+	],
+	separator: '_',  
+	directories: 'single',
+	namingConvention: 'date',
+	inputFields: ['picsee', 'photo']
+}
+
+picsee.initialize(options);
+
+/**
+ * Mocha Tests
+ */
+describe('picsee', function() {
+	
+	it('should be an object', function() { 
+		picsee.should.be.a('object');
+	});
+
+	it('should upload an image', function() { 
+		picsee.upload('...')
+	});
+	
+	it('should reject a file that is NOT an image', function() { });
+	
+	it('should remove staging files if rejected', function() {});
+	it('should remove staging files if uploaded to be processed', function() {});
+
+	it('should crop an uploaded photo', function() {});
+	it('should create new versions of the cropped photo based on specs', function() {});
+	it('should remove the preprocessed photo if cropped and versioned', function() {});
+
+});
